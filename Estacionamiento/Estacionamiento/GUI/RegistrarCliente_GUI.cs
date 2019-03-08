c747cd9fde3221922d30052015d9534edffb31ba
saritapech15@gmail.com <saritapech15@gmail.com>
2019-03-07 22:50:49 -0600

    Una form de Sarai
    una gui de cliente nada mas

---
+++ Estacionamiento/Estacionamiento/GUI/RegistrarCliente_GUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estacionamiento.GUI
+{
+	public partial class RegistrarCliente_GUI : Form
+	{
+		public RegistrarCliente_GUI()
+		{
+			InitializeComponent();
+		}
+	}
+}
